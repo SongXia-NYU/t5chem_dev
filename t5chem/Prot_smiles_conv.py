47b5dea38a743afb20adfaecae601907633ad55b
tkella47 <46170696+tkella47@users.noreply.github.com>
2022-12-22 18:13:37 -0500

    changed behavior of modifcation so each molecule contained

--- t5chem/Prot_smiles_conv.py
+++ t5chem/Prot_smiles_conv.py
@@ -2,8 +2,17 @@ from rdkit.Chem.rdmolfiles import MolFromFASTA,MolToSmiles
 from numpy import random
 
 def AAtoSmiles(sequence):
-	mol_obj = MolFromFASTA(sequence)
-	return MolToSmiles(mol_obj)
+	mol_objects = []
+	for aa in sequence:
+		mol_objects.append(MolFromFASTA(aa))
+	smiles_string = ""
+	for molecule in mol_objects:
+		smiles_string += "<mod>"
+		smiles_string += MolToSmiles(molecule)
+		smiles_string += "</mod>"
+	return smiles_string
+
+
 def pick_random_partition(line):
 	start = random.randint(0, len(line))
 	end = random.randint(start, start + 5)
@@ -14,6 +23,7 @@ def pick_random_partition(line):
 	return start, end
 
 def write_mixed_file(origin_file,new_file, chance=0.25):
+	remove_org = False
 	file_mix_tag = open(new_file, "w")
 	lines = [line.strip() for line in open(origin_file)]
 	for line in lines:
@@ -23,13 +33,18 @@ def write_mixed_file(origin_file,new_file, chance=0.25):
 			smiles = AAtoSmiles(line[start:end])
 			if "<mod>" + smiles + "</mod>" == "<mod></mod>":
 				print(start, end)
-			smiles = "<mod>" + smiles + "</mod>"
+			#smiles = "<mod>" + smiles + "</mod>"
 			left_partition = line[0:start]
 			right_partition = line[end:len(line)]
 			modified_line = left_partition + smiles + right_partition
-		if modified_line:
-			file_mix_tag.write(modified_line + "\n")
+		if remove_org:
+			if modified_line:
+				file_mix_tag.write(modified_line + "\n")
+			else:
+				file_mix_tag.write(line + "\n")
 		else:
+			if modified_line:
+				file_mix_tag.write(modified_line + "\n")
 			file_mix_tag.write(line + "\n")
 
 	print("finished")
